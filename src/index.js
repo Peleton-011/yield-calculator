32a94a59b55005fbb3065509b2638eb53ea38ca1
Nico <nicodiazan@gmail.com>
2023-03-27 22:03:36 +0200

    Actually fix react-webpack conflict

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
 root.render(
     <React.StrictMode>
         <App />
